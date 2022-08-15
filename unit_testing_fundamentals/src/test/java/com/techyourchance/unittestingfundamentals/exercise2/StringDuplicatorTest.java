5172b832828f4eead6755e69f5d96a55cc6ad431
Nam Anh Nguyen <namanh11611@gmail.com>
2022-08-15 21:51:17 +0700

    Do exercise 2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,29 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringDuplicatorTest {
+
+    StringDuplicator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void test_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        Assert.assertThat(result, CoreMatchers.is(""));
+    }
+
+    @Test
+    public void test_normalString_duplicateStringReturned() {
+        String result = SUT.duplicate("This is test");
+        Assert.assertThat(result, CoreMatchers.is("This is testThis is test"));
+    }
 
 }
