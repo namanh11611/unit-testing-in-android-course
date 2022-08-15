46528031297c3da897f8191a71c88948558f36f2
Nam Anh Nguyen <namanh11611@gmail.com>
2022-08-15 21:37:21 +0700

    Do exercise 1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1() {
+        boolean result = SUT.isNegative(-1);
+        Assert.assertThat(result, CoreMatchers.is(true));
+    }
+
+    @Test
+    public void test2() {
+        boolean result = SUT.isNegative(0);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
+
+    @Test
+    public void test3() {
+        boolean result = SUT.isNegative(1);
+        Assert.assertThat(result, CoreMatchers.is(false));
+    }
 
 }
